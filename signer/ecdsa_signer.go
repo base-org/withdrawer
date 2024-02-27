7b7f3d7ee8dd378338475b27ce3de553217b64d7
Maksym Pyvovarov <119981087+maxpv026@users.noreply.github.com>
2024-02-28 00:21:40 +0200

    Update ecdsa_signer.go
    added comments to describe each method and rephrased some variable names for clarity.

--- signer/ecdsa_signer.go
+++ signer/ecdsa_signer.go
@@ -10,23 +10,29 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
+// ecdsaSigner represents an ECDSA signer.
 type ecdsaSigner struct {
 	*ecdsa.PrivateKey
 }
 
+// Address returns the address associated with the ECDSA signer.
 func (s *ecdsaSigner) Address() common.Address {
 	return crypto.PubkeyToAddress(s.PublicKey)
 }
 
+// SignerFn returns a signer function using the ECDSA private key and chain ID.
 func (s *ecdsaSigner) SignerFn(chainID *big.Int) bind.SignerFn {
 	return opcrypto.PrivateKeySignerFn(s.PrivateKey, chainID)
 }
 
+// SignData signs the given data using the ECDSA private key.
 func (s *ecdsaSigner) SignData(data []byte) ([]byte, error) {
 	sig, err := crypto.Sign(crypto.Keccak256(data), s.PrivateKey)
 	if err != nil {
 		return nil, err
 	}
+	// Adjust the recovery ID for Ethereum compatibility
 	sig[crypto.RecoveryIDOffset] += 27
-	return sig, err
+	return sig, nil
 }
+
